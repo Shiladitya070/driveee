411bd7d18b48f222a6dbceddda1bfa0423150b08
shiladitya <shiladityad940@gmail.com>
2023-08-05 02:16:56 +0530

    upload btn

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { SignOutButton, currentUser } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 import React from "react";
 
@@ -7,13 +8,18 @@ async function NavBar() {
   const user = await currentUser();
 
   return (
-    <div className="navbar bg-primary text-primary-content shadow-xl ">
+    <div className="navbar bg-primary text-primary-content shadow-xl align-middle">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Driveee</a>
+        <Link href={"/"} className="btn btn-ghost normal-case text-xl">
+          Driveee
+        </Link>
       </div>
       {user ? (
-        <div className="flex-none">
+        <div className="flex items-center">
           <div className="dropdown dropdown-end">
+            <Link href={"/upload"}>
+              <button className="btn btn-secondary">Upload</button>
+            </Link>
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <Image
